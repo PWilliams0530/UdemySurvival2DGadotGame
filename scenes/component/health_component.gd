e04eea2e8a096eb31e4fc0077149eead91bb1253
PWilliams0530 <PWilliams0530@gmail.com>
2024-04-13 16:02:59 -0400

    addedVialDropCounter

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -11,6 +11,9 @@ func _ready():
 func damage(damage_amount: float):
 	#takes higher of the current health or 0 to prevent negative health
 	current_health = max(current_health - damage_amount, 0)
+	Callable(check_death).call_deferred()
+
+func check_death():
 	if current_health == 0:
 		died.emit()
 		#removes root node of scene
