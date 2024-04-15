a2b038f68173aad45d6b5c5976dc20bf8cf9b4d7
PWilliams0530 <PWilliams0530@gmail.com>
2024-04-15 15:41:43 -0400

    AddedUpgradeScreenTest

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var upgrade_pool : Array[AbilityUpgrade]
 @export var experience_manager : Node
+@export var upgrade_screen_scene : PackedScene
 
 var current_upgrades = {}
 
@@ -13,13 +14,24 @@ func on_level_up(current_level : int):
 	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
 	if chosen_upgrade == null:
 		return
+		
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
+		
+	print(current_upgrades)
+		
 	
-	var has_upgrade = current_upgrades.has(chosen_upgrade.Id)
+func apply_upgrade(upgrade: AbilityUpgrade):
+	var has_upgrade = current_upgrades.has(upgrade.Id)
 	if !has_upgrade:
-		current_upgrades[chosen_upgrade.Id] = {
-			"resource" : chosen_upgrade,
+		current_upgrades[upgrade.Id] = {
+			"resource": upgrade,
 			"quantity" : 1
 		}
 	else:
-		current_upgrades[chosen_upgrade.Id]["quantity"] += 1
+		current_upgrades[upgrade.Id]["quantity"] += 1
 	
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
