9624f6e097b039bba6de1e19d316de887c9a15f4
PWilliams0530 <PWilliams0530@gmail.com>
2024-04-29 10:02:14 -0400

    AddedThemeToUpgradeScreens

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -47,7 +47,6 @@ func update_upgrade_pool(chosen_upgrade : AbilityUpgrade):
 
 func pick_upgrades():
 	var chosen_upgrades : Array[AbilityUpgrade] = []
-
 	for i in 2:
 		if upgrade_pool.items.size() == chosen_upgrades.size():
 			break;
