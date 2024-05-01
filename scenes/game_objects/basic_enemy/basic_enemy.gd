975c842eadac7a4c1b8d1e754dcfe88d782dcee0
PWilliams0530 <PWilliams0530@gmail.com>
2024-05-01 13:05:06 -0400

    AddedAbilityToHitEnemyOnce

--- scenes/game_objects/basic_enemy/basic_enemy.gd
+++ scenes/game_objects/basic_enemy/basic_enemy.gd
@@ -6,6 +6,9 @@ extends CharacterBody2D
 # Called when the node enters the scene tree for the first time.
 #func _ready():
 	#$Area2D.area_entered.connect(on_area_entered)
+	
+func _ready():
+	$HurtBoxComponent.hit.connect(on_hit)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,3 +24,5 @@ func _process(delta):
 #func on_area_entered(other_area: Area2D):
 	#health_component.damage(100)
 	
+func on_hit():
+	$AudioStreamPlayer2D.play()
