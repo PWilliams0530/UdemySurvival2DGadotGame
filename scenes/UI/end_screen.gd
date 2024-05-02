bbf17dbbc1d1b1c318a75ac065a682d6bce3d626
PWilliams0530 <PWilliams0530@gmail.com>
2024-05-02 19:20:45 -0400

    AddedFoundationForSaveDataUpgrades

--- scenes/UI/end_screen.gd
+++ scenes/UI/end_screen.gd
@@ -31,4 +31,6 @@ func on_restart_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 	
 func on_quit_button_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
 	get_tree().quit()
