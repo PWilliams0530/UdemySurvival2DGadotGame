44c4f443ee16d84173911942caae95f3682f8e45
PWilliams0530 <PWilliams0530@gmail.com>
2024-05-07 16:50:12 -0400

    FixedDaggerAbilityError

--- scenes/ability/dagger_ability/NorthArea.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-func on_body_entered(body):
-	print("north body entered")
-	queue_free()
-	%North.queue_free()
--- scenes/ability/dagger_ability/NorthCollision.gd
+++
@@ -1,5 +0,0 @@
-extends CollisionShape2D
-
-
-func _on_north_hitbox_area_entered(area):
-	queue_free()
